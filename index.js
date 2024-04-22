d7a0a892be61538366d3b5a69d842f48b104d813
Harsh Vardhan <devharsh2k4@gmail.com>
2024-04-23 03:13:12 +0530

    final commit

--- index.js
+++ index.js
@@ -1,46 +1,22 @@
 const mongoose = require("mongoose")
 const express = require('express')
-const Product = require('./models/product.model');
+const productRoute = require("./routes/product.route.js");
 
 
 const app = express()
 
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use("/api/products", productRoute);
+
 
 app.get('/', function (req, res) {
     res.send('Root')
     console.log("/ called");
 })
-
-app.get('/api/products', async (req, res,) => {
-    try {
-        const products = await Product.find();
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-app.get('/api/products/:id', async (req, res) => {
-    try {
-const {id} = req.params;
-const product = await Product.findById(id);
-res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
-
-
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
 
 
 mongoose.connect("mongodb+srv://devharsh2k4:qNW37OpvGj87xHL4@cruddb.uljq7gc.mongodb.net/node-api?retryWrites=true&w=majority&appName=CrudDB")
